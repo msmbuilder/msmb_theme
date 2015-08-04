00a7dce2beae13d466b1a24f8d0df490103709b8
Matthew Harrigan <harrigan@stanford.edu>
2015-08-04 13:20:52 -0700

    No tag prefix

--- msmb_theme/_version.py
+++ msmb_theme/_version.py
@@ -36,7 +36,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = """"
+    cfg.tag_prefix = ""
     cfg.parentdir_prefix = "msmb_theme-"
     cfg.versionfile_source = "msmb_theme/_version.py"
     cfg.verbose = False

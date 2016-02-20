057b14ce11f8211e905f00d138b9ed8b1c6aa2d9
Matthew Harrigan <harrigan@stanford.edu>
2016-02-19 18:05:08 -0800

    Display versions by new "display" field in versions.json

--- msmb_theme/static/js/versions.js
+++ msmb_theme/static/js/versions.js
@@ -3,14 +3,14 @@
 // get all of the releases from versions.json, and use these to populate the
 // dropdown menu of different releases
 $(document).ready(function () {
-    // Define base_url in versions.html, probably through a template variable
+    // Define versions_json_url in versions.html through a template variable
     $.getJSON(versions_json_url)
         .done(function (data) {
             $.each(data.sort(function (a, b) {
                 return a.version > b.version
             }), function (i, item) {
                 $("<dd>").append(
-                    $("<a>").text(item.version).attr('href', item.url)
+                    $("<a>").text(item.display).attr('href', item.url)
                 ).appendTo("#versionselector");
             });
         })
